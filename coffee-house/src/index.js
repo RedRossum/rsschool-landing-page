15b24a22a5f968365df9811a2db284a670019ae0
RedRossum <redrossum@gmail.com>
2023-12-05 17:03:34 +0300

    fix: fix files for multi html

--- coffee-house/src/index.js
+++ coffee-house/src/index.js
@@ -1,11 +1,11 @@
 import './scss/main.scss';
 import './index.html';
-import './scss/pages/second.html';
+// import './scss/pages/menu.html';
 
 const linkPage = document.querySelector('.our-page')
 
 function moveLocation() {
-    window.location.href = "./src/scss/pages/second.html'"
+    window.location.href = 'menu.html';
 }
 
 linkPage.addEventListener('click', moveLocation)
---
+++ coffee-house/src/scss/pages/index.js
@@ -0,0 +1,2 @@
+import '../main.scss';
+// import './menu.html';

386ae63dddde7bafa8c89f5affadc1f392ec03bf
RedRossum <redrossum@gmail.com>
2023-12-11 18:44:42 +0300

    feat: add burger on main/menu page

--- coffee-house/src/index.js
+++ coffee-house/src/index.js
@@ -2,6 +2,7 @@ import './scss/main.scss';
 import './index.html';
 // import './scss/pages/menu.html';
 
+
 const linkPage = document.querySelector('.our-page');
 const linkPageS = document.querySelector('.animation');
 
@@ -11,3 +12,30 @@ function moveLocation() {
 
 linkPage.addEventListener('click', moveLocation);
 linkPageS.addEventListener('click', moveLocation);
+
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+const blackout = document.querySelector('.blackout');
+
+ hamburger.addEventListener('click', function () {
+    navigation.classList.toggle('active');
+    hamburger.classList.toggle('open');
+    blackout.classList.toggle('hidden');
+    document.querySelector('body').classList.toggle('scroll-disabled');
+    navigation.addEventListener('click', function (event) {
+        if (event.target.classList.contains('navigation__link')) {
+            hamburger.classList.remove('open');
+            navigation.classList.remove('active');
+            blackout.classList.add('hidden');
+            document.querySelector('body').classList.remove('scroll-disabled');
+        }
+    });
+    blackout.addEventListener('click', function (event) {
+        if (event.target.classList.contains('blackout')) {
+            hamburger.classList.remove('open');
+            navigation.classList.remove('active');
+            blackout.classList.add('hidden');
+            document.querySelector('body').classList.remove('scroll-disabled');
+        }
+    });
+});
--- coffee-house/src/scss/pages/index.js
+++ coffee-house/src/scss/pages/index.js
@@ -1,2 +1,32 @@
 import '../main.scss';
 // import './menu.html';
+
+const menu = document.querySelector('.menu');
+menu.classList.toggle('active');
+
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+const blackout = document.querySelector('.blackout');
+
+hamburger.addEventListener('click', function () {
+    navigation.classList.toggle('active');
+    hamburger.classList.toggle('open');
+    blackout.classList.toggle('hidden');
+    document.querySelector('body').classList.toggle('scroll-disabled');
+    navigation.addEventListener('click', function (event) {
+        if (event.target.classList.contains('navigation__link')) {
+            hamburger.classList.remove('open');
+            navigation.classList.remove('active');
+            blackout.classList.add('hidden');
+            document.querySelector('body').classList.remove('scroll-disabled');
+        }
+    });
+    blackout.addEventListener('click', function (event) {
+        if (event.target.classList.contains('blackout')) {
+            hamburger.classList.remove('open');
+            navigation.classList.remove('active');
+            blackout.classList.add('hidden');
+            document.querySelector('body').classList.remove('scroll-disabled');
+        }
+    });
+});

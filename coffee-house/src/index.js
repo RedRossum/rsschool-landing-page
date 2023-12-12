7bbf10b5db21150a5df6dd2a215c9989787ba4d4
RedRossum <redrossum@gmail.com>
2023-12-12 21:39:10 +0300

    feat: add basic slider

--- coffee-house/src/index.js
+++ coffee-house/src/index.js
@@ -12,12 +12,12 @@ function moveLocation() {
 
 linkPage.addEventListener('click', moveLocation);
 linkPageS.addEventListener('click', moveLocation);
-
+//burger
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
 const blackout = document.querySelector('.blackout');
 
- hamburger.addEventListener('click', function () {
+hamburger.addEventListener('click', function () {
     navigation.classList.toggle('active');
     hamburger.classList.toggle('open');
     blackout.classList.toggle('hidden');
@@ -38,4 +38,52 @@ const blackout = document.querySelector('.blackout');
             document.querySelector('body').classList.remove('scroll-disabled');
         }
     });
+});
+
+//slider
+const sliderControlRight = document.querySelector('.right');
+const sliderControlLeft = document.querySelector('.left');
+const carousel = document.querySelector('.carousel')
+const rightCard = document.querySelector("#right-card");
+const leftCard = document.querySelector("#left-card");
+const activeCard = document.querySelector('#active-card');
+
+function renderCard(imgPath, title, text, price) {
+    let cardImg
+}
+
+function moveRight() {
+    carousel.classList.add('move-right');
+    sliderControlRight.removeEventListener('click', moveRight);
+    sliderControlLeft.removeEventListener('click', moveLeft);
+}
+
+function moveLeft() {
+    carousel.classList.add('move-left');
+    sliderControlLeft.removeEventListener('click', moveLeft);
+    sliderControlRight.removeEventListener('click', moveRight);
+}
+
+sliderControlRight.addEventListener('click', moveRight);
+sliderControlLeft.addEventListener('click', moveLeft);
+
+
+carousel.addEventListener('animationend', function (event) {
+    let stage;
+    if (event.animationName === 'roll-right') {
+        carousel.classList.remove('move-right');
+        stage = activeCard.innerHTML;
+        activeCard.innerHTML = rightCard.innerHTML;
+        rightCard.innerHTML = leftCard.innerHTML;
+        leftCard.innerHTML = stage;
+    } else {
+        carousel.classList.remove('move-left');
+        stage = activeCard.innerHTML;
+        activeCard.innerHTML =  leftCard.innerHTML;
+        leftCard.innerHTML = rightCard.innerHTML;
+        rightCard.innerHTML = stage;
+    }
+
+    sliderControlRight.addEventListener('click', moveRight);
+    sliderControlLeft.addEventListener('click', moveLeft);
 });
